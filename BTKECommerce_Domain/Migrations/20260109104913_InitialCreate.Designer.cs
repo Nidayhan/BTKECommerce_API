d78513d44a3a95f4434ec9d0f757dc243bd62c6b
Nidayhan <nida0ayhan@gmail.com>
2026-01-09 19:35:47 +0300

    migration işlemi yapıldı

--- BTKECommerce_Domain/Migrations/20260109104913_InitialCreate.Designer.cs
+++ BTKECommerce_Domain/Migrations/20260109104913_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BTKECommerce_Domain.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260104092950_addedLogCs")]
-    partial class addedLogCs
+    [Migration("20260109104913_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- BTKECommerce_Domain/Migrations/20260109104913_InitialCreate.cs
+++ BTKECommerce_Domain/Migrations/20260109104913_InitialCreate.cs
@@ -6,11 +6,28 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace BTKECommerce_Domain.Migrations
 {
     /// <inheritdoc />
-    public partial class initialize : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "AppLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Level = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Method = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppLogs", x => x.Id);
+                });
+
             migrationBuilder.CreateTable(
                 name: "AspNetRoles",
                 columns: table => new
@@ -60,6 +77,7 @@ namespace BTKECommerce_Domain.Migrations
                     Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
                     UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
                 },
@@ -247,6 +265,9 @@ namespace BTKECommerce_Domain.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "AppLogs");
+
             migrationBuilder.DropTable(
                 name: "AspNetRoleClaims");
 
